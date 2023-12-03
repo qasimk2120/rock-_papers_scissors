a5d6256f0eb521fb230d1f40e26860fe1d4151e3
qasimk2120 <qk71086@gmail.com>
2023-12-04 00:53:29 +0300

    rock paper scissors game web

---
+++ app.js
@@ -0,0 +1,82 @@
+const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+let gameIsRunning = false;
+
+//pLAYER Choice prompt and save + coversion to uppercase function expression
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+//computer choice generator and a function expression
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW //expression before : is run if conditon is truthy
+    : (cChoice === ROCK && pChoice == PAPER) || //and to the right of : is run if condition is false
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+//   if (cChoice === pChoice) {
+//     return RESULT_DRAW;
+//   } else if (
+//     (cChoice === ROCK && pChoice == PAPER) ||
+//     (cChoice === PAPER && pChoice === SCISSORS) ||
+//     (cChoice === SCISSORS && pChoice === ROCK)
+//   ) {
+//     return RESULT_PLAYER_WINS;
+//   } else {
+//     return RESULT_COMPUTER_WINS;
+//   }
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
+  winner === RESULT_DRAW
+    ? (message = message + 'had a draw')
+    : winner === RESULT_PLAYER_WINS
+    ? (message = message + 'won')
+    : (message = message + 'lost');
+  alert(message);
+  gameIsRunning = false;
+
+  //   if(winner === RESULT_DRAW){
+  //     message = message + 'had a draw'
+  //   }else if (winner ===RESULT_PLAYER_WINS){
+  //     message  = message + 'won';
+  //   }
+  //   else{
+  //     message = message + 'lost';
+  //   }
+});
